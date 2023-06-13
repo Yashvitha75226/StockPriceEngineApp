7d8acf645845c7b7e36c6c113d1e6b9cad9ff0bc
Yashvitha Beedala <y.beedala@zensar.com>
2023-06-13 10:53:45 +0530

    first commit

---
+++ src/main/java/com/stocks/StockPriceEngineApp/StockPriceEngineAppApplication.java
@@ -0,0 +1,13 @@
+package com.stocks.StockPriceEngineApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPriceEngineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPriceEngineAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stocks/StockPriceEngineApp/StockPriceEngineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocks.StockPriceEngineApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPriceEngineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
